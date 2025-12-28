1687a2afff1c5d39bbb0abe4ebb01db5c36c9178
Sultanbek <zambulovsultanbek@gmail.com>
2025-12-28 12:26:48 +0500

    Remove base configuration from Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,6 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
-        base: "./",
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
